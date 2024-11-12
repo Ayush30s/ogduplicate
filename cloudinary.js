8277ec189c9a18085c8c7dfd57c19a20e722a686
Ayush Srivastav <139707217+Ayush30s@users.noreply.github.com>
2024-11-12 12:02:59 +0530

    Update cloudinary.js

--- cloudinary.js
+++ cloudinary.js
@@ -1,15 +1,15 @@
 const cloudinary = require("cloudinary").v2;
 
-const fun = async(path) => {
-    cloudinary.config({ 
-        cloud_name: 'dzblab9we', 
-        api_key: '575212773332514', 
-        api_secret: 'ghj2avDgbHGv81YNAzo95uvpnHg'
-    });
-    
-    const uploadResult = await cloudinary.uploader.upload(path);
-    return uploadResult;
-}
+const fun = async (path) => {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.ghj2avDgbHGv81YNAzo95uvpnHg,
+  });
+
+  const uploadResult = await cloudinary.uploader.upload(path);
+  return uploadResult;
+};
 
 // fun("./Public/uploads/1725900255150download (4).jpg");
 module.exports = fun;
